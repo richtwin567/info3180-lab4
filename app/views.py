4d1962734d59366efe73b9dc95cd395c3d3f1309
richtwin567 <anakai.richards@gmail.com>
2021-02-23 18:19:34 -0500

    prevent unauthorized access to files

--- app/views.py
+++ app/views.py
@@ -66,6 +66,8 @@ def get_image(filename):
 
 @app.route("/files")
 def files():
+    if not session.get('logged_in'):
+        abort(401)
     return render_template("files.html",files=get_uploaded_images())
 
 @app.route('/login', methods=['POST', 'GET'])
